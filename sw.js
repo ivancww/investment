afc1dc2c6c1cad0caad81a9efbabac05310c4605
ivancww <wahchiu0903@gmail.com>
2026-05-03 01:12:43 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'jig-pwa-v1.2.0';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/images/office-view.jpg',
+  '/images/dse-compare.jpg',
+  '/images/2212-path.jpg',
+  '/images/aia-success.jpg',
+  '/images/icon-192.png',
+  '/images/icon-512.png'
+];
+
+// 安裝並緩存資源
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching assets...');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 攔截請求，優先從緩存讀取
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// 清理舊緩存
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
